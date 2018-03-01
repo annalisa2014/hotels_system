9dc2b2725a09e1df954fa1fae5ea0c72fd166021
Annalisa Bacherotti <a.bacherotti@eggzero.it>
2018-03-02 00:04:44 +0100

    SHowing hotels only to authorized users

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,10 +1,12 @@
 class HotelsController < ApplicationController
+  before_action :authenticate
 
   def index
     @hotels = Hotel.all
   end
 
   def show
+    render_forbidden unless is_user_hotel_manager?(params[:id])
     HotelViewsCountJob.perform_later(params[:id])
     @hotel = Hotel.find(params[:id])
     render :json => @hotel, status: 200
@@ -18,6 +20,10 @@ class HotelsController < ApplicationController
     @hotel = Hotel.new(hotel_params)
 
     if @hotel.save
+      authenticate_with_http_token do |token, options|
+        user = User.find_by(token: token)
+        user.managers.create(hotel: @hotel)
+      end
       redirect_to @hotel
     else
       render 'new'
@@ -25,12 +31,13 @@ class HotelsController < ApplicationController
   end
 
   def edit
+    render_forbidden unless is_user_hotel_manager?(params[:id])
     @hotel = Hotel.find(params[:id])
   end
 
   def update
+    render_forbidden unless is_user_hotel_manager?(params[:id])
     @hotel = Hotel.find(params[:id])
-
     if @hotel.update(hotel_params)
       redirect_to @hotel
     else
@@ -39,9 +46,9 @@ class HotelsController < ApplicationController
   end
 
   def destroy
+    render_forbidden unless is_user_hotel_manager?(params[:id])
     @hotel = Hotel.find(params[:id])
     @hotel.destroy
-
     redirect_to hotels_path
   end
 
@@ -49,4 +56,24 @@ class HotelsController < ApplicationController
   def hotel_params
     params.require(:hotel).permit(:name, :description, :country_code, :average_price)
   end
+
+  def render_forbidden
+    self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+    render :json => 'Bad credentials', status: 403
+  end
+
+  def validate_user
+    authenticate_user_by_hotel || render_forbidden
+  end
+
+  def is_user_hotel_manager?(hotel)
+    authenticate_with_http_token do |token, options|
+      user = User.find_by(token: token)
+      if user.manager
+        submitted_hotel = Hotel.find(hotel)
+        user.hotels.include?(submitted_hotel)
+      end
+    end
+  end
+
 end

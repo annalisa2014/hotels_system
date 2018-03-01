cf5db1d842ce6e2fc89c6e297ffe499a2f171151
Annalisa Bacherotti <a.bacherotti@eggzero.it>
2018-03-01 21:38:14 +0100

    Adding manager field to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180301141254) do
+ActiveRecord::Schema.define(version: 20180301182514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,13 +36,14 @@ ActiveRecord::Schema.define(version: 20180301141254) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "password"
-    t.string "salt"
-    t.string "language"
-    t.string "token"
+    t.string  "first_name"
+    t.string  "last_name"
+    t.string  "email"
+    t.string  "password"
+    t.string  "salt"
+    t.string  "language"
+    t.string  "token"
+    t.boolean "manager"
   end
 
 end

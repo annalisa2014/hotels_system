1ec92476603e4a7844f182001a30940fe1a13763
Annalisa Bacherotti <a.bacherotti@eggzero.it>
2018-03-03 11:14:03 +0100

    BUg fix for job counting hotels views

--- app/jobs/hotel_views_count_job.rb
+++ app/jobs/hotel_views_count_job.rb
@@ -2,7 +2,7 @@ class HotelViewsCountJob < ActiveJob::Base
   queue_as :default
 
   def perform(args)
-    hotel = Hotel.find(args.first)
+    hotel = Hotel.find(args)
     hotel.increment!(:views_count)
   end
 end
